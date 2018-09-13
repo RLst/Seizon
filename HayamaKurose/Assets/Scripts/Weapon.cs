268ee90d6d04a6e637c0f4b668ac166e62a679fe
Tony L <letony@icloud.com>
2018-09-13 14:49:55 +1000

    Basic Weapons class

---
+++ HayamaKurose/Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Seizon {
+
+	public enum WeaponType {
+		MELEE,
+		HAND_GUN,
+		ASSAULT_RIFLE
+	}
+
+	public class Weapon : MonoBehaviour {
+
+		public float range;		//in Metres; Range of the weapon
+		public float damage;	//in Hitpoints; Max damage that will be dealt by the weapon
+		public WeaponType type;
+	}
+
+}
