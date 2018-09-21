6da893ce07991296e1270e095d2eb782d09386e0
Tony Le <letony@icloud.com>
2018-09-21 17:09:57 +1000

    Spawners working, gun working, GUI half working

--- HayamaKurose/Assets/Scripts/AI/SpawnManager.cs
+++ HayamaKurose/Assets/Scripts/AI/SpawnManager.cs
@@ -8,7 +8,7 @@ public class SpawnManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		SpawnPositions = GameObject.FindGameObjectsWithTag("Spawner");
 	}
 	
 	// Update is called once per frame
