d73cae30d33950e4c630a7ca4f129f82a1db864f
Nathan Paonessa <nathan.a.p@hotmail.com>
2018-09-21 15:41:57 +1000

    UI Overlay Sceen Added

---
+++ HayamaKurose/Assets/Scripts/UI/Overlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay : MonoBehaviour {
+
+    public Text m_HP;
+    public Text m_Timer;
+    public Text m_KillCount;
+    public Text m_Round;
+    public Text m_ClipRemaining;
+    public Text m_TotalAmmo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
