58be5362c28a3a26ae0e94abd5ac497f6d56b39f
Tony Le <letony@icloud.com>
2018-09-26 16:46:03 +1000

    Started implementation for Pickups

---
+++ HayamaKurose/Assets/Pickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Seizon
+{
+
+public class Pickup : MonoBehaviour {
+
+	private Player player;  //The object that can pick this up
+
+	//What does this pickup actually add?
+	private GameObject pickup;
+
+
+}
+
+}
