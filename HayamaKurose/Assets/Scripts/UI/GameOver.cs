4e1902687a27ebdb75eacf543a10bfbef80dceb0
Nathan Paonessa <nathan.a.p@hotmail.com>
2018-09-26 17:35:51 +1000

    Added Game Over screen with functionality

---
+++ HayamaKurose/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Seizon {
+    public class GameOver : MonoBehaviour {
+        public Text DaysSurvived;
+        public Text HayamasKilled;
+        private GameController GC;
+        // Use this for initialization
+        void Start() {
+            GC = FindObjectOfType<GameController>();
+            DaysSurvived.text = "You survived " + GC.dayCount + " day(s)";
+            HayamasKilled.text = "You killed " + GC.killCount + " Hayama(s)";
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void TryAgainButtonOnPress()
+        {
+            SceneManager.LoadScene("Main");
+            Time.timeScale = 1;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        public void MainMenuButtonOnPress()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
